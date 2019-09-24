b6ed91cbd74964cf7575411998af663a66e4074c
rico-coelen <rico.coelen@gmail.com>
2019-09-24 18:23:50 +0200

    changes to enemy

---
+++ Assets/Scripts/AttackScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+    private float timeToAttack;
+    public float cooldownAttack;
+
+    public Transform attackPos;
+    public float attackRange;
+
+    public LayerMask Enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeToAttack <= 0)
+        {
+            if(Input.GetKey(KeyCode.E))
+            {
+                timeToAttack = cooldownAttack;
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, Enemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+
+                }
+            }
+        }
+        else
+        {
+            timeToAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float health = 100f;
+    public float speed;
+
+    private Animator anim;
+
+    public bool facingRight = true;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+            // check velocity and let player stay left after walking
+        if (rb.velocity.x > 0 && !facingRight || rb.velocity.x < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Destroy(this);
+        }
+        // maybe show health above head?
+        //healthBar.value = Mathf.Clamp(health, 0, 100f);
+    }
+}
